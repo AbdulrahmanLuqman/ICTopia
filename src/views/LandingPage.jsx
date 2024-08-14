f4d157996089855d30eef3128c22a40cc5e91f1c
AbdulrahmanLuqman <fal528@dervac.com>
2024-08-14 11:34:27 +0100

    first commit

---
+++ src/views/LandingPage.jsx
@@ -0,0 +1,11 @@
+import HeroSection from "../components/LandingPageComponents/HeroSection"
+
+const LandingPage = () => {
+  return (
+    <main>
+        <HeroSection />
+    </main>
+  )
+}
+
+export default LandingPage
