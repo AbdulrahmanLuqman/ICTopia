ccf19d293359f7e56affe115a352aec8e8d007c5
unknown <you@example.com>
2024-08-14 13:58:04 +0100

    created services sectiction

--- src/views/LandingPage.jsx
+++ src/views/LandingPage.jsx
@@ -1,9 +1,11 @@
 import HeroSection from "../components/LandingPageComponents/HeroSection"
+import ServicesSection from "../components/LandingPageComponents/ServicesSection/ServicesSection"
 
 const LandingPage = () => {
   return (
     <main>
         <HeroSection />
+        <ServicesSection />
     </main>
   )
 }
