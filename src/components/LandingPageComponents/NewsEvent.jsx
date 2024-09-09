040bbcdbb1f223fccab48680a61d80b309040e37
Webwiz <aliuanas004@gmail.com>
2024-09-09 18:50:37 +0100

    public

--- src/components/LandingPageComponents/NewsEvent.jsx
+++ src/components/LandingPageComponents/NewsEvent.jsx
@@ -3,7 +3,7 @@ import { FaFacebookF, FaLinkedinIn, FaPhoneAlt, FaTwitter } from "react-icons/fa
 import { LiaLinkSolid } from "react-icons/lia";
 import { IoMdMail } from "react-icons/io";
 import { MdOutlineLocationOn } from "react-icons/md";
-import location from "../../../public/images/location.png"
+import location from "../../../public/location.png"
 
 // import { GoDotFill } from "react-icons/go"
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,11 @@ export default {
     extend: {
     backgroundImage: {
       'service-img': "url('/src/service-bg.svg')",
-      news: 'url(./images/news.jpg)',
-      event1: 'url(./images/event1.jpg)',
-      event2: 'url(./images/event2.jpg)',
-      event3: 'url(./images/event3.jpg)',
-      map: 'url(./images/map.jpg)',
+      news: 'url(/public/news.jpg)',
+      event1: 'url(/public/event1.jpg)',
+      event2: 'url(/public/event2.jpg)',
+      event3: 'url(/public/event3.jpg)',
+      map: 'url(/public/map.jpg)',
     },
   },
   plugins: [require('daisyui')],
