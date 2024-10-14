a8ad3ba9305bd9ad82b8aa58f75c79c9bc5a49be
Abdulaganiyu-Abdulazeez <abdulganiyuabdulazeez20@gmail.com>
2024-10-14 05:25:46 +0100

    second copany commit

--- src/components/ServicesComponents/OurCompany.jsx
+++ src/components/ServicesComponents/OurCompany.jsx
@@ -3,10 +3,10 @@ const OurCompany = () => {
     <div className="w-full grid lg:grid-cols-4  md:grid-cols-2 grid-cols-1 gap-[2rem">
       {
         <div className="flex flex-col items-cemter text-center text-white">
-          <div className="text-white hover:text-[#54c4cf]">company.image</div>
-          <p className="font-[400] font-[20px]">company.title</p>
+          <div className="text-white hover:text-[#54c4cf]">{company.image}</div>
+          <p className="font-[400] font-[20px]">{company.title}</p>
           <p className="w-[5rem] h-[3px] bg-[#54c4cf]"></p>
-          <p className="text-[20px]">company.content</p>
+          <p className="text-[20px]">{company.content}</p>
         </div>
       }
     </div>
